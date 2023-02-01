bb48de84f03d166d87cc7ca68b18c683b5523758
Gordon Kirsch <me@gordonkirsch.dev>
2023-02-01 13:17:51 +0100

    Initial Commit

---
+++ WakeOnLan/WakeOnLanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeOnLanApp.swift
+//  WakeOnLan
+//
+//  Created by Gordon on 01.02.23.
+//
+
+import SwiftUI
+
+@main
+struct WakeOnLanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
